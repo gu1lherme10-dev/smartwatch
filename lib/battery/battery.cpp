cac18fada8b08b8c4b49c15cc1911352a3a48e65
Guilherme da Silva <guilhermesilva.compasso@natura.net>
2025-02-26 17:01:26 -0300

    feat: remove comments

--- lib/battery/battery.cpp
+++ lib/battery/battery.cpp
@@ -4,50 +4,50 @@
 #include <utils.h>
 
 // Private variables
-AXP20X_Class power;  // Instancia o objeto AXP20X_Class para o AXP202
+AXP20X_Class power; 
 unsigned long startMillisBattery;
 unsigned long currentMillisBattery;
-const unsigned long period = 2000;  // Intervalo de 2 segundos
+const unsigned long period = 2000;  
 float batteryAverageVoltage = 0.00;
 
 void setupBatteryMonitor()
 {
     Wire.begin();
-    power.begin(); // Inicializa a comunicação I2C com o AXP202
+    power.begin(); 
 
-    if (!power.begin())  // Verifica se o AXP202 foi inicializado corretamente
+    if (!power.begin()) 
     {
         Serial.println("Falha ao inicializar o AXP202!");
-        while (1);  // Para o código se não conseguir inicializar o AXP202
+        while (1); 
     }
 
-    power.setPowerOutPut(AXP202_LDO2, AXP202_ON);  // Habilita a saída LDO2
-    power.setLDO2Voltage(3300);  // Define a tensão do LDO2 para 3.3V
-    power.setChgLEDMode(AXP20X_LED_LOW_LEVEL);  // Habilita o carregamento da bateria
-    // power.enableACIN();  // Habilita a entrada de alimentação externa, se necessário
+    power.setPowerOutPut(AXP202_LDO2, AXP202_ON); 
+    power.setLDO2Voltage(3300);
+    power.setChgLEDMode(AXP20X_LED_LOW_LEVEL);
+    
 
-    // Inicializa o filtro de média móvel
-    initializeMovingAverage(10);  // Inicializa com 10 amostras para média móvel
-    startMillisBattery = millis();  // Marca o tempo inicial
+   
+    initializeMovingAverage(10);  
+    startMillisBattery = millis();  
 }
 
 void calculateBatteryVoltage()
 {
-    // Obtém a tensão da bateria usando o AXP202
+    
     float batteryVoltage = power.getBattVoltage();
-    batteryAverageVoltage = movingMediaFilter(batteryVoltage);  // Aplica o filtro de média
+    batteryAverageVoltage = movingMediaFilter(batteryVoltage);  
 }
 
 int getBatteryPercentage()
 {
-    if (isChargingBattery())  // Se a bateria estiver carregando
+    if (isChargingBattery())  
     {
-        float batRef = 4.2;  // Tensão de referência da bateria LiPo (4.2V totalmente carregada)
-        return map(batteryAverageVoltage, 3.0, batRef, 0, 100);  // Mapeia a tensão para uma porcentagem de 0-100%
+        float batRef = 4.2;  
+        return map(batteryAverageVoltage, 3.0, batRef, 0, 100);  
     }
     else
     {
-        return -1;  // Bateria não conectada ou não carregando
+        return -1; 
     }
 }
 
@@ -71,23 +71,23 @@ int getBatteryStatus()
     }
     else if (getBatteryPercentage() > 95)
     {
-        return 4;  // Bateria quase cheia
+        return 4;
     }
     return -1;  // Caso de erro
 }
 
 bool isChargingBattery()
 {
-    return power.isChargeing();  // Função do AXP202 que verifica o status de carregamento
+    return power.isChargeing();
 }
 
 void loopMonitoringBattery()
 {
     currentMillisBattery = millis();
-    if (currentMillisBattery - startMillisBattery >= period)  // Se passaram 2 segundos
+    if (currentMillisBattery - startMillisBattery >= period)
     {
-        calculateBatteryVoltage();  // Atualiza a tensão da bateria
-        startMillisBattery = currentMillisBattery;  // Reinicia o tempo
+        calculateBatteryVoltage(); 
+        startMillisBattery = currentMillisBattery; 
     }
 }
 
